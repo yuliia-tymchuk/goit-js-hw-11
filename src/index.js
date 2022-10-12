6d70ecefd87c0c5cf922d47d0c7da6d8334d2843
lia2307 <juliatymchuk23@gmail.com>
2022-10-12 22:10:59 +0300

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
