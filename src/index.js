d7dd3118a0e4ae973119efbab21ffbbb66fd57c1
lia2307 <juliatymchuk23@gmail.com>
2022-10-12 23:03:58 +0300

    done markup

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { refs } from './js/refs';
+import { PixabayAPI } from './js/pixabayAPI';
+import { createMarkup } from './js/createMarkup';
---
+++ src/js/pixabayAPI.js
@@ -0,0 +1,47 @@
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+axios.defaults.headers.common['Authorization'] = 'Client-ID u_79wq4chor4';
+
+class PixabayAPI {
+  #page = 1;
+  #query = '';
+  #totalPages = 0;
+  #perPage = 30;
+  #params = {
+    params: {
+      key: '30554717 - dcc80d5299a215984c1269f52',
+      q: 'all images',
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+    },
+  };
+
+  async getPhotos() {
+    const urlAXIOS = `/search/photos?page=${this.#page}&query=${this.#query}`;
+    const { data } = await axios.get(urlAXIOS, this.#params);
+    return data;
+  }
+
+  set query(newQuery) {
+    this.#query = newQuery;
+  }
+
+  get query() {
+    return this.#query;
+  }
+
+  incrementPage() {
+    this.#page += 1;
+  }
+
+  resetPage() {
+    this.#page = 1;
+  }
+  calculateTotalPages(total) {
+    this.#totalPages = Math.ceil(total / this.#perPage);
+  }
+
+  get isShowLoadMore() {
+    return this.#page < this.#totalPages;
+  }
+}
