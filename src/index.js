5f4df9d63d0e6f1597168f5f6abf4c595728d719
lia2307 <juliatymchuk23@gmail.com>
2022-10-13 22:31:32 +0300

    updated pixabayApi

--- src/index.js
+++ src/index.js
@@ -6,3 +6,106 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { refs } from './js/refs';
 import { PixabayAPI } from './js/pixabayAPI';
 import { createMarkup } from './js/createMarkup';
+
+const pixabay = new PixabayAPI();
+
+const options = {
+  root: null,
+  rootMargin: '100px',
+  threshold: 1.0,
+};
+const callback = async function (entries, observer) {
+  entries.forEach(async entry => {
+    // if (entry.isIntersecting && entry.intersectionRect.bottom > 550) {
+    if (entry.isIntersecting) {
+      pixabay.incrementPage();
+      observer.unobserve(entry.target);
+
+      try {
+        const { results } = await pixabay.getPhotos();
+
+        const markup = createMarkup(results);
+
+        refs.list.insertAdjacentHTML('beforeend', markup);
+        if (pixabay.isShowLoadMore) {
+          const target = document.querySelector('.gallery__item:last-child');
+          io.observe(target);
+        }
+      } catch (error) {
+        Notify.failure(error.message, 'Щось пішло не так!');
+        clearPage();
+      }
+    }
+  });
+};
+const io = new IntersectionObserver(callback, options);
+
+const handleSubmit = async event => {
+  event.preventDefault();
+
+  const {
+    elements: { query },
+  } = event.currentTarget;
+  const searchQuery = query.value.trim().toLowerCase();
+  if (!searchQuery) {
+    Notify.failure('Ввдедіть дані для пошуку!!!');
+    return;
+  }
+  pixabay.query = searchQuery;
+
+  clearPage();
+
+  try {
+    const { results, total } = await pixabay.getPhotos();
+    if (results.length === 0) {
+      Notify.info(`За вашим запитом
+ ${searchQuery} зображень не знайдено!
+`);
+      return;
+    }
+    const markup = createMarkup(results);
+    refs.list.insertAdjacentHTML('beforeend', markup);
+
+    pixabay.calculateTotalPages(total);
+
+    Notify.success(`Ми знайшли ${total} зображень по запиту ${searchQuery}`);
+
+    if (pixabay.isShowLoadMore) {
+      const target = document.querySelector('.gallery__item:last-child');
+      console.log(target);
+
+      io.observe(target);
+    }
+  } catch (error) {
+    Notify.failure(error.message, 'Щось пішло не так!');
+    clearPage();
+  }
+};
+
+const onLoadMore = () => {
+  pixabay.incrementPage();
+
+  if (!pixabay.isShowLoadMore) {
+    refs.loadMoreBtn.classList.add('is-hidden');
+  }
+
+  unsplash
+    .getPhotos()
+    .then(({ results }) => {
+      const markup = createMarkup(results);
+      refs.list.insertAdjacentHTML('beforeend', markup);
+    })
+    .catch(error => {
+      Notify.failure(error.message, 'Щось пішло не так!');
+      clearPage();
+    });
+};
+
+refs.form.addEventListener('submit', handleSubmit);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function clearPage() {
+  pixabay.resetPage();
+  refs.list.innerHTML = '';
+  refs.loadMoreBtn.classList.add('is-hidden');
+}
--- src/js/pixabayAPI.js
+++ src/js/pixabayAPI.js
@@ -1,27 +1,36 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://pixabay.com/api/';
-axios.defaults.headers.common['Authorization'] = 'Client-ID u_79wq4chor4';
+// axios.defaults.baseURL = 'https://pixabay.com/api/';
+// axios.defaults.headers.common['Authorization'] =
+//   'key 30554717-dcc80d5299a215984c1269f52';
 
 export class PixabayAPI {
   #page = 1;
   #query = '';
   #totalPages = 0;
   #perPage = 40;
-  #params = {
-    params: {
-      key: '30554717-dcc80d5299a215984c1269f52',
-      q: 'all images',
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: 'true',
-    },
-  };
+  // #params = {
+  //   params: {
+  //     key: '30554717-dcc80d5299a215984c1269f52',
+  //     q: '',
+  //     image_type: 'photo',
+  //     orientation: 'horizontal',
+  //     safesearch: 'true',
+  //   },
+  // };
 
-  async getPhotos() {
-    const urlAXIOS = `/search/photos?page=${this.#page}&query=${this.#query}`;
-    const { data } = await axios.get(urlAXIOS, this.#params);
-    return data;
+  getPhotos() {
+    const url = `https://pixabay.com/api/?key=30554717-dcc80d5299a215984c1269f52&q=${
+      this.#query
+    }&image_type=photo&page=${this.#page}&per_page=${
+      this.#perPage
+    }&orientation=horizontal&safesearch=true`;
+    return fetch(url).then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+        return response.json();
+      }
+    });
   }
 
   set query(newQuery) {
