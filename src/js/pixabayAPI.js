7b0c49c540533d88f60bb64e49034abd254da9f8
lia2307 <juliatymchuk23@gmail.com>
2022-10-19 21:47:35 +0300

    Update pixabayAPI.js

--- src/js/pixabayAPI.js
+++ src/js/pixabayAPI.js
@@ -1,36 +1,29 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://pixabay.com/api/';
-// axios.defaults.headers.common['Authorization'] =
-//   'key 30554717-dcc80d5299a215984c1269f52';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const KEY = '30554717-dcc80d5299a215984c1269f52';
 
 export class PixabayAPI {
   #page = 1;
   #query = '';
   #totalPages = 0;
   #perPage = 40;
-  // #params = {
-  //   params: {
-  //     key: '30554717-dcc80d5299a215984c1269f52',
-  //     q: '',
-  //     image_type: 'photo',
-  //     orientation: 'horizontal',
-  //     safesearch: 'true',
-  //   },
-  // };
+  #params = {
+    params: {
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      per_page: '40',
+    },
+  };
 
-  getPhotos() {
-    const url = `https://pixabay.com/api/?key=30554717-dcc80d5299a215984c1269f52&q=${
+  async getPhotos() {
+    const url = `https://pixabay.com/api/?key=${KEY}&q=${
       this.#query
-    }&image_type=photo&page=${this.#page}&per_page=${
-      this.#perPage
-    }&orientation=horizontal&safesearch=true`;
-    return fetch(url).then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-        return response.json();
-      }
-    });
+    }&image_type=photo&page=${this.#page}`;
+
+    const { data } = await axios.get(url, this.#params);
+    return data;
   }
 
   set query(newQuery) {
